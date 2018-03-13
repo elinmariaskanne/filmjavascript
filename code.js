5a68be9f07091caa6f283be3f7134c039bfcbaa0
elinmariaskanne <elin@skanne.se>
2018-03-13 14:36:08 +0100

    changes

---
+++ code.js
@@ -0,0 +1,52 @@
+function renderMovie(data){
+    $(".infosection h1").text(data.title);   
+    $(".infosection p").text(data.review);
+    $(".poster").attr("src", data.imgUrl);
+  
+  $(".infosection ul").empty();
+  for(let i=0; i<data.actors.length; i++){
+    $(".infosection ul").append("<li>" + data.actors[i] + "</li>");
+  }
+}
+
+    
+function changeStarRating(rating){
+      $(".filled").removeClass("filled");
+      for(let i=1; i<=rating; i++){
+        $("[data-rating-id=" + i + "]").addClass("filled");
+      }
+    }
+
+
+$(".stars").on("click", "span", function(e){
+  let star = $(e.target);
+  let rating = parseInt(star.attr("data-rating-id"));
+  changeStarRating(rating);
+});
+
+renderMovie(movieData);
+
+  //   let actorList = "";
+  //   for(let i=0; i<data.actors.length; i++){
+  //     actorList += "<li>" + data.actors[i] + "</li>";
+  //   }
+  //   document.querySelector(".infosection ul").innerHTML = actorList;
+  // }
+
+  // function changeStarRating(rating){
+  //   for(let i=1; i<=5; i++){
+  //     let star = document.getElementById("star" + i);
+  //     if (i <= rating){
+  //       star.classList.add("filled");
+  //     } else {
+  //       star.classList.remove("filled");
+  //     }
+  //   }  
+  // }
+  
+  // for(let i=1; i<=5; i++){
+  //   let star = document.getElementById("star" + i);
+  //   star.addEventListener("click", function(){
+  //     changeStarRating(i); 
+  //   });
+  // }
---
+++ code2.js
@@ -0,0 +1,6 @@
+function renderMovie(data) {
+
+    document.querySelector()
+
+}
+
