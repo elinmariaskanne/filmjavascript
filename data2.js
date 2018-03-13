d2260e1af0fac3a2a7f17ee4eb8daefd0c214f4b
elinmariaskanne <elin@skanne.se>
2018-03-13 16:40:38 +0100

    andringar

--- data2.js
+++ data2.js
@@ -3,4 +3,4 @@ module.exports = {
     posterUrl = "https://static.rogerebert.com/uploads/movie/movie_poster/inception-2010/large_ziKvu3Th9l1wN2aIeVj5ElpBqFu.jpg",
     movieInfo = "A very meta movie about dreams within dreams. And then there was a whole bunch of shooting, not really sure how that fit into the picture.",
     actors = ["Leonardo DiCaprio", "Ellen Page", "Tom Hardy"]
-}
+};
